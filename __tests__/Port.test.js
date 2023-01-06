94904b64d43a606b6568c9ded76b91e451ada5f2
Abi Turner <turner.abi@gmail.com>
2023-01-06 16:08:20 +1000

    completed isolating with dummies on port and itinerary tests

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -22,7 +22,7 @@ describe('Port', () => {
         })
 
         it('can add a ship', () => {
-            const ship = {};
+            const ship = jest.fn();
 
             port.addShip(ship);
 
@@ -30,8 +30,8 @@ describe('Port', () => {
         })
     
         it('can remove a ship', () => {
-            const titanic = {};
-            const queenMary = {};
+            const titanic = jest.fn();
+            const queenMary = jest.fn();
 
             port.addShip(titanic);
             port.addShip(queenMary);
