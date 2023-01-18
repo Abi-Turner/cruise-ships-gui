cdb28b72d73998f90d9efb262fcad1b704e10d30
Abi Turner <turner.abi@gmail.com>
2023-01-19 07:22:05 +1100

    set up cruise ship gui project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
+
+json { "extends": "mcr-codes", "env": { "browser": true } }
---
+++ src/Controller.js
@@ -0,0 +1,34 @@
+(function exportController() {
+  function Controller() {
+    this.initialiseSea();
+  }
+
+  Controller.prototype.initialiseSea = function initialiseSea() {
+    const backgrounds = ['./images/water0.png', './images/water1.png'];
+    let backgroundIndex = 0;
+    window.setInterval(() => {
+      document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+      backgroundIndex += 1;
+    }, 1000);
+  };
+
+  Controller.prototype = {
+    renderPorts(ports) {
+      const portsElement = document.querySelector('#ports');
+      portsElement.style.width = '0px';
+    },
+
+    ports.forEach((port, index) => {
+      const newPortElement = document.createElement('div');
+      newPortElement.className = 'port';
+
+      portsElement.appendChild(newPortElement);
+    })
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Controller;
+  } else {
+    window.Controller = Controller;
+  }
+}())
